c917b9fe97d470d618bc18d846268c4f4d343342
[김호준] <[rlaghwns126@naver.com]>
2021-03-30 16:57:00 +0900

    React메인코드

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  render(){
+  
+  return (
+    <div className="App">
+      Hello, React!!
+    </div>
+   );
+  }
+}
+
+export default App;
