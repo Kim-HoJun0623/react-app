41f8b489686694a771da37f88a7e6060f77f609b
[김호준] <[rlaghwns126@naver.com]>
2021-04-06 14:01:24 +0900

    state,props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Component } from 'react';
-import { render } from '@testing-library/react';
-class Subject extends Component{
-  render(){
-    return (
-      <header>
-        <h1>WEB</h1>
-         world wide web!
-      </header>
-    );
-  }  
-}
-class TOC extends Component{
-  render(){
-    return(
-    <nav>
-      <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-      </ul>
-  </nav>);
-  }
-  
-}
-class Content extends Component{
-  render(){
-    return(
-          <article>  
-             <h2>HTML</h2>
-              HTML is HyperText Markup Language.
-          </article>
-    );
-  }
+import TOC from "./components/TOC";
+import Content from"./components/Content";
+import Subject from"./components/Subject";
 
-}
 
 class App extends Component {
+
+    constructor(props){
+      super(props);
+      this.state={
+        mode:'read',    
+        Subject:{title:'WEB',sub:'World wid Web!'},
+        welcome:{title:'welcome',desc:'Hello, React!!'},
+        contents:[
+          {id:1, title:'HTML',desc:'HTML is for infromation'},
+          {id:2, title:'CSS',desc:'CSS is for design'},
+          {id:3, title:'JavaScript',desc:'JavaScript is for interactive'}
+
+        ]
+      }
+
+    }
+
   render(){
-  
+    console.log('App render')
+  var _title,_desc=null;
+    if(this.state.mode === 'welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+
+  }else if (this.state.mode === 'read'){
+    _title =this.state.contents[0].title;
+    _desc =this.state.contents[0].desc;
+
+  }
   return (
     <div className="App">
-      <Subject></Subject>
-      <TOC></TOC>
-      <Content></Content>
+      {/* <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject title="React" sub="For UI"></Subject> */}
+      <Subject
+        title={this.state.Subject.title}
+        sub={this.state.Subject.sub}></Subject>
+      <TOC data={this.state.contents}></TOC>
+      <Content title={_title} desc={_desc}></Content>
     </div>
    );
   }
